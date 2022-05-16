777bec4f49ecf79554112fac85960b9bacee8f9d
Kaushal2701 <kaushalswami7178@gmail.com>
2022-05-16 14:50:32 +0530

    team added

--- assets/navigation.php
+++ assets/navigation.php
@@ -5,7 +5,7 @@
     <div id="navbar-collapse-1" class="navbar-collapse collapse">
         <ul class="nav navbar-nav">
             <li class="dropdown"><a href="home.php">Home<span class="nav-subtitle">OUR World</span></a></li>
-            <li class="dropdown"> <a href="#">Our Team<span class="nav-subtitle">Meet us!</span></a></li>
+            <li class="dropdown"> <a href="team.php">Our Team<span class="nav-subtitle">Meet us!</span></a></li>
             <li class="dropdown"> <a href="https://smu.edu.in/smit.html/" target="_blank">SMITian<span class="nav-subtitle">our personal blog</span></a></li>
             <!--<li class="dropdown"> <a href="http://updates.collegespace.in/" target="_blank">Updates<span class="nav-subtitle">Our web bulletin</span></a></li>-->
             <li><a href="contact.php">CONTACT<span class="nav-subtitle">say us hi</span></a></li>
